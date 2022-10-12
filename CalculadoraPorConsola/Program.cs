8100d5c6b2d8feb1cb4ab2201fc3536e17ce5e16
William <fpmw.50@gmail.com>
2022-10-12 00:05:22 -0500

    Creacion de nuevo proyecto Calculadora por consola

---
+++ CalculadoraPorConsola/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraPorConsola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double numero1=0, numero2=0, suma, resta, multi, divi;
+            Console.WriteLine("Ingresa el numero 1: ");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingresa el numero 2: ");
+            numero2 = Convert.ToInt32(Console.ReadLine());
+
+            suma = numero1 + numero2;
+            resta = numero1 - numero2;
+            multi = numero1 * numero2;
+            divi = numero1 / numero2;
+
+            Console.WriteLine("Suma=  "+ suma);
+            Console.WriteLine("Resta=  " + resta);
+            Console.WriteLine("Multi=  " + multi);
+            Console.WriteLine("Divi=  " + divi);
+            Console.ReadLine();
+        }
+    }
+}
